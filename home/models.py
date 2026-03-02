4ba207450352c6691c18b08dab8f525272aedf04
Khadija Khatun <khadija.khatun@codencoit.com>
2026-03-03 00:16:41 +0600

    Add custom User model extending AbstractUser with role field

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
-# Create your models here.
+class User(AbstractUser):
+    ROLES_CHOICES = [
+        ('admin','admin'),
+        ('doctor','doctor'),
+        ('paitent','paitent'),
+        ('staff','staff')
+    ]
+    role = models.CharField(max_length=20,choices=ROLES_CHOICES)
