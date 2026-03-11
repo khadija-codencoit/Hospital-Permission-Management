da8c15fac383861b6f9847cdec652faf23ed46af
khadija-codencoit <khadija.khatun@codencoit.com>
2026-03-11 14:53:58 +0600

    Add user registration API in AuthViewSet

--- home/views.py
+++ home/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from .models import *
 from .serializers import *
 
 # Create your views here.
+
+class AuthViewSet(viewsets.ViewSet):
+
+    def register(self,request):
+        serializers = RegistrationSerializer(data = request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response()
+
 
 
